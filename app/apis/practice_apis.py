4f945d71bdf50ccddbb454acf531092451a0b996
kimeunmi82 <blesseunmi82@gmail.com>
2026-07-14 16:40:43 +0900

    add practice_apis.py

---
+++ app/apis/practice_apis.py
@@ -0,0 +1,55 @@
+# app/apis/practice_apis.py
+user_list = [
+	{
+		"id": 1,
+		"name": "홍길동",
+		"age": 24,
+		"email": "gildong24@example.com",
+		"password": "Password1234!!"
+	},
+	{
+		"id": 2,
+		"name": "장문복",
+		"age": 21,
+		"email": "moonluck12@example.com",
+		"password": "Check1321!"
+	},
+	{
+		"id": 3,
+		"name": "임우진",
+		"age": 31,
+		"email": "limousine33@example.com",
+		"password": "lwsPAssword12@"
+	}
+]
+
+# task01 - 정현우
+
+
+
+
+
+
+# task02 - 정현우
+
+
+
+
+
+
+# task03 - 홍승완
+
+
+
+
+
+
+# task04 - 김지혜
+
+
+
+
+
+
+# task05 - 김은미
+
